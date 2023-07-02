8b68405c16c85a5e2794067c8a05d6bac99a5afb
Ol1mowski <olimarki123@wp.pl>
2023-07-02 16:48:08 +0200

    add rwd to second section and add scroll delay to button

--- src/components/HomeSite/HomeSiteButton/HomeSiteButton.js
+++ src/components/HomeSite/HomeSiteButton/HomeSiteButton.js
@@ -1,14 +1,22 @@
+import { Link } from "react-scroll";
 import style from "./HomeSiteButton.module.scss";
 const HomeSiteButton = () => {
       
 
     return (
         <div>
-            <a href="#more">
-                <button className={style.button}>
-                    Learn more
-                </button>
-            </a>
+    <Link
+        activeClass="active"
+        to="more"
+        spy={true}
+        smooth={true}
+        offset={-70}
+        duration={1200}
+    >
+        <button className={style.button}>
+            Learn more
+        </button>
+      </Link>
         </div>
     );
 };
--- src/components/WelcomeToHotel/WelcomeToHotel.js
+++ src/components/WelcomeToHotel/WelcomeToHotel.js
@@ -5,13 +5,17 @@ const WelcomeToHotel = () => {
 
     return(
         <div className={style.container} id='more'>
-            <h2 className={style.header}>
-                Welcome to your <br /> luxurious home away <br /> from home 
-            </h2>
-            <span className={style.content}>
-                Write a paragraph that talks about your brand or property here. Convince your prospective clients to choose you and your offerings by highlighting the qualities that set you apart from the competition. Your audience is already on your website, so push a little bit harder to seal the deal!
-            </span>
-            <img loading='lazy' className={style.img} src={img} alt="Happy client"/>
+            <div className={style.strings}>
+                <h2 className={style.header}>
+                    Welcome to your <br /> luxurious home away <br /> from home 
+                </h2>
+                <span className={style.content}>
+                    Write a paragraph that talks about your brand or property here. Convince your prospective clients to choose you and your offerings by highlighting the qualities that set you apart from the competition. Your audience is already on your website, so push a little bit harder to seal the deal!
+                </span>
+            </div>
+            <div className={style.image}>
+                <img loading='lazy' className={style.img} src={img} alt="Happy client"/>
+            </div>
         </div>
     )
 }
