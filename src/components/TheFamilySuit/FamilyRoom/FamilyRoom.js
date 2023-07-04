5e8f9ae09d1f89b369561570298ec4a34d4809db
Ol1mowski <olimarki123@wp.pl>
2023-07-04 16:16:08 +0200

    add RWD to 3-th section

--- src/components/TheFamilySuit/FamilyRoom/FamilyRoom.js
+++ src/components/TheFamilySuit/FamilyRoom/FamilyRoom.js
@@ -5,8 +5,8 @@ const FamilyRoom = () => {
     return(
         <div>
             <div className={style.Wrapper}>
-                <img className={style.img} src={room} alt='room' />
-                <div className={style.contentWrapper}>
+                <img id={style.firstImg} className={style.img} src={room} alt='room' />
+                <div id={style.firstBorder} className={style.contentWrapper}>
                     <span className={style.content}>
                         An intriguing caption that describes the room goes here.
                         Use a flattering photo, then describe away!
--- src/components/TheFamilySuit/TheFamilySuit.js
+++ src/components/TheFamilySuit/TheFamilySuit.js
@@ -6,13 +6,15 @@ const TheFamilySuit = () => {
     return(
         <div className={style.container}>
             <div className={style.string}>
-                <h2 className={style.header}>The Family Suit</h2>
+                <h2 className={style.header}>The Family<br /> Suit</h2>
                 <span className={style.description}>Showcase the best your property has to offer by highlighting one of your accommodations. Add a flattering photo, then describe the room's best feature.</span>
             </div>
             <div className={style.img}>
                 <FamilyRoom />
             </div>
-            <Furniture />
+            <div className={style.furniture}>
+                <Furniture />
+            </div>
         </div>
     )
 }
