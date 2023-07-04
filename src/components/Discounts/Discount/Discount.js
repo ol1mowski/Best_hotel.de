b9d7147f0a772c2ad333b524f540353beac4af61
Ol1mowski <olimarki123@wp.pl>
2023-07-04 16:45:36 +0200

    refactoring component with props

--- src/components/Discounts/Discount/Discount.js
+++ src/components/Discounts/Discount/Discount.js
@@ -1,35 +1,15 @@
 import style from './Discount.module.scss';
-import bookOnline from '../../../assets/pictures/booking_online.png';
-import spa from '../../../assets/pictures/spa.png';
-import tourist from '../../../assets/pictures/tourist_with_suitcase.png';
 
-const Discount = () => {
+const Discount = (props) => {
+
     return(
         <div className={style.container}>
             <div className={style.Wrapper}>
-            <img className={style.img} src={bookOnline} alt='booking online photo' />
+            <img className={style.img} src={props.img} alt='booking online photo' />
                 <div className={style.contentWrapper}>
                     <span className={style.content}>
-                    <h3 className={style.contentHeader}>Early Bird Discount </h3>
-                    <span className={style.contentBelow}>List your offers, promos, or special membership privileges and perks here to entice people to book your property.</span> 
-                    </span>
-                </div>
-            </div>
-            <div className={style.Wrapper}>
-            <img className={style.img} src={spa} alt='spa photo' />
-                <div className={style.contentWrapper}>
-                    <span className={style.content}>
-                    <h3 className={style.contentHeader}>Wellhall Members Club</h3>
-                    <span className={style.contentBelow}>List your offers, promos, or special membership privileges and perks here to entice people to book your property.</span> 
-                    </span>
-                </div>
-            </div>
-            <div className={style.Wrapper}>
-            <img className={style.img} src={tourist} alt='spa photo' />
-                <div className={style.contentWrapper}>
-                    <span className={style.content}>
-                    <h3 className={style.contentHeader}>Book 3 Nights,Get 1 Night Free</h3>
-                    <span className={style.contentBelow}>List your offers, promos, or special membership privileges and perks here to entice people to book your property.</span> 
+                    <h3 className={style.contentHeader}>{props.title}</h3>
+                    <span className={style.contentBelow}>{props.more}</span> 
                     </span>
                 </div>
             </div>
--- src/components/Discounts/Discounts.js
+++ src/components/Discounts/Discounts.js
@@ -1,11 +1,51 @@
 import Discount from './Discount/Discount';
 import style from './Discounts.module.scss';
+import bookOnline from '../../assets/pictures/booking_online.png';
+import spa from '../../assets/pictures/spa.png';
+import tourist from '../../assets/pictures/tourist_with_suitcase.png';
 
 const Discounts = () => {
+
+    const discount = [
+        {
+            id: 1,
+            title: 'Early Bird Discount',
+            more: 'List your offers, promos, or special membership privileges and perks here to entice people to book your property.',
+            img: bookOnline,
+        },
+        {
+            id: 2,
+            title: 'Wellhall Members Club',
+            more: 'List your offers, promos, or special membership privileges and perks here to entice people to book your property.',
+            img: spa,
+        },
+        {
+            id: 3,
+            title: 'Book 3 Nights,Get 1 Night Free',
+            more: 'List your offers, promos, or special membership privileges and perks here to entice people to book your property.',
+            img: tourist,
+        },
+
+    ]
+
     return(
         <div className={style.container}>
             <h2 className={style.header}>Promos and Offers</h2>
-            <Discount />
+            <Discount 
+            title={discount[0].title}
+            more={discount[0].more}
+            img={discount[0].img}
+            />
+            <Discount 
+            title={discount[1].title}
+            more={discount[1].more}
+            img={discount[1].img}
+            />
+            <Discount 
+            title={discount[2].title}
+            more={discount[2].more}
+            img={discount[2].img}
+            />
         </div>
     )
 }
