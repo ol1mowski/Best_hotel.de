71091b006fcc62ab07438248524676fee5c4b3e2
Ol1mowski <olimarki123@wp.pl>
2023-07-05 18:46:59 +0200

    add RWD to 5-th section

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -2,28 +2,56 @@ import style from'./Comment.module.scss';
 import backgroundPhoto from '../../assets/pictures/hotel_coridor.png';
 import Post from './Post/Post';
 
-const background = {
-    position: 'relative',
-    backgroundImage: `url(${backgroundPhoto})`,
-    backgroundSize: 'cover',
-    backgroundRepeat: 'no-repeat',
-    backgroundPosition: 'center',
-    width: '100vw',
-    height: '140vh',
-    display: 'block',
-    filter: 'contrast(60%)',
-};
+// const background = {
+//     backgroundImage: `url(${backgroundPhoto})`,
+//     backgroundSize: 'cover',
+//     backgroundRepeat: 'no-repeat',
+//     backgroundPosition: 'center',
+//     width: '100vw',
+//     height: '140vh',
+//     display: 'block',
+//     // filter: 'contrast(60%)',
+// };
 
-const wrapper = {
-    position: 'relative',
-}
+
+const posts = [
+    {
+        id: 1,
+        user: 'Santa Solana Post',
+        content: "Boost your product and service's credibility by adding testimonials from your clients. People love recommendations so feedback from others who've tried it is invaluable.",
+    },
+    {
+        id: 2,
+        user: "Mariana's Luxe Travels",
+        content: "Boost your product and service's credibility by adding testimonials from your clients. People love recommendations so feedback from others who've tried it is invaluable.",
+    },
+    {
+        id: 3,
+        user: "Fairhill Journal",
+        content: "Boost your product and service's credibility by adding testimonials from your clients. People love recommendations so feedback from others who've tried it is invaluable.",
+    },
+]
 
 const Comment = () => {
     return(
-        <div style={wrapper}>
-            <div style={background}></div>
+        <div className={style.mainWrapper}>
+            <div className={style.background}>
             <h2 className={style.content}>In the Press</h2>
-            <Post />
+                <div className={style.main}>
+                    <Post 
+                    user={posts[0].user}
+                    content={posts[0].content}
+                    /> 
+                    <Post 
+                    user={posts[1].user}
+                    content={posts[1].content}
+                    /> 
+                    {/* <Post 
+                    user={posts[2].user}
+                    content={posts[2].content}
+                    />  */}
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/Comment/Post/Post.js
+++ src/components/Comment/Post/Post.js
@@ -1,38 +1,18 @@
 import style from'./Post.module.scss';
 import quote from'../../../assets/icons/quote.svg';
 
-const Post = () => {
+const Post = (props) => {
     return(
         <div className={style.wrapper}>
             <img className={style.quote} src={quote} alt='Quote' />
             <p className={style.content}>
-                Boost your product and service's credibility by adding testimonials from your clients. People love recommendations so feedback from others who've tried it is invaluable.
+                {props.content}
             </p>
             <p className={style.content}>
                 - 
             </p>
             <p className={style.content}>
-                Santa Solana Post
-            </p>
-            <img className={style.quote} src={quote} alt='Quote' />
-            <p className={style.content}>
-                Boost your product and service's credibility by adding testimonials from your clients. People love recommendations so feedback from others who've tried it is invaluable.
-            </p>
-            <p className={style.content}>
-                - 
-            </p>
-            <p className={style.content}>
-                Mariana's Luxe Travels
-            </p>
-            <img className={style.quote} src={quote} alt='Quote' />
-            <p className={style.content}>
-                Boost your product and service's credibility by adding testimonials from your clients. People love recommendations so feedback from others who've tried it is invaluable.
-            </p>
-            <p className={style.content}>
-                - 
-            </p>
-            <p className={style.content}>
-            Fairhill Journal
+                {props.user}
             </p>
         </div>
     )
