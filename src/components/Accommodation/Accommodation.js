f23ae20aaf1f6121119a17a6b01f65730b98de25
Ol1mowski <olimarki123@wp.pl>
2023-07-07 10:56:55 +0200

    add animation to accomodation section

--- src/components/Accommodation/Accommodation.js
+++ src/components/Accommodation/Accommodation.js
@@ -1,14 +1,39 @@
+import React, { useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
+import { motion, useAnimation } from 'framer-motion';
 import style from './Accommodation.module.scss';
 import Room from './Room/Room';
 
 const Accommodation = (props) => {
+  const controls = useAnimation();
+  const [ref, inView] = useInView({
+    triggerOnce: true, // Animacja zostanie uruchomiona tylko raz
+  });
 
-    return(
-        <div className={style.container}>
-            <p className={style.header}>Accommodation</p>
-            <Room hotel={props.hotel}/>
-        </div>
-    )
-}
+  useEffect(() => {
+    if (inView) {
+      controls.start('visible');
+    }
+  }, [controls, inView]);
+
+  const containerVariants = {
+    hidden: { opacity: 0, y: 50 },
+    visible: { opacity: 1, y: 0, transition: { duration: 1 } },
+  };
+
+  return (
+    <motion.div
+      className={style.container}
+      id="accommodation"
+      ref={ref}
+      variants={containerVariants}
+      initial="hidden"
+      animate={controls}
+    >
+      <p className={style.header}>Accommodation</p>
+      <Room hotel={props.hotel} />
+    </motion.div>
+  );
+};
 
 export default Accommodation;
